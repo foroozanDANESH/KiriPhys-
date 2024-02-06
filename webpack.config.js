e0b337517874b8047ba0cb0b2d363388e76beaf0
Forozan Danesh <forozandanesh@Forozans-MacBook-Pro.local>
2024-02-06 14:10:56 -0800

    Updated webpack configuration and fixed build issues

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,3 @@
-// run webpack-dev-server --content-base dist
-// https://webpack.github.io/docs/configuration.html
 const webpack = require('webpack');
 const path = require('path');
 
@@ -10,35 +8,40 @@ module.exports = {
         filename: '[name].bundle.js',
         publicPath: '/assets/'
     },
-    devtool: '#source-map',
+    devtool: 'source-map',
     devServer: {
         contentBase: './dist'
     },
     module: {
         rules: [
             {
-                test: /\.(js|jsx)$/,
+                test: /\.js$/,
                 exclude: /node_modules/,
                 use: {
                     loader: 'babel-loader',
                     options: {
-                        presets: [
-                            "@babel/preset-env",
-                            "@babel/preset-react"
-                        ]
+                        presets: ['@babel/preset-env', '@babel/preset-react']
                     }
                 }
             },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
             { test: /\.json$/, loader: 'json-loader' },
-            { test: /\.css$/, loader: 'style-loader!css-loader' },
-            { test: /\.woff(\d+)?$/, loader: 'url?prefix=font/&limit=5000&mimetype=application/font-woff' },
-            { test: /\.ttf$/, loader: 'file?prefix=font/' },
-            { test: /\.eot$/, loader: 'file?prefix=font/' },
-            { test: /\.svg$/, loader: 'file?prefix=font/' },
-            { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'url-loader?limit=10000&minetype=application/font-woff' },
-            { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'file-loader' },
-            { test: require.resolve('jquery'), loader: 'expose?jQuery!expose?$' }
-        ]
+            { test: /\.woff(\d+)?$/, use: 'url-loader?prefix=font/&limit=5000&mimetype=application/font-woff' },
+            { test: /\.ttf$/, use: 'file-loader?prefix=font/' },
+            { test: /\.eot$/, use: 'file-loader?prefix=font/' },
+            { test: /\.svg$/, use: 'file-loader?prefix=font/' },
+            { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, use: 'url-loader?limit=10000&minetype=application/font-woff' },
+            { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, use: 'file-loader' },
+            {
+                test: require.resolve('jquery'),
+                use: [
+                  { loader: 'expose-loader', options: { exposes: ['jQuery', '$'] } }
+                ]
+              }
+                      ]
     },
     plugins: [
         new webpack.ProvidePlugin({
