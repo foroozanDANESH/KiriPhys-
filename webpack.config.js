25e8e99104c819783413891530f2dc472552ceae
Forozan Danesh <forozandanesh@Forozans-MacBook-Pro.local>
2024-02-06 12:27:40 -0800

    Fix Webpack configuration syntax error

--- webpack.config.js
+++ webpack.config.js
@@ -14,20 +14,20 @@ module.exports = {
     devServer: {
         contentBase: './dist'
     },
-module: {
-  rules: [
-    {
-      test: /\.(js|jsx)$/,
-      exclude: /node_modules/,
-      use: {
-        loader: 'babel-loader',
-                query: {
-  "presets": [
-    "@babel/preset-env",
-    "@babel/preset-react"
-  ]
-}
-                include: path.join(__dirname, '/src')
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            "@babel/preset-env",
+                            "@babel/preset-react"
+                        ]
+                    }
+                }
             },
             { test: /\.json$/, loader: 'json-loader' },
             { test: /\.css$/, loader: 'style-loader!css-loader' },
