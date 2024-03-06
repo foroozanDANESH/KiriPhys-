9b794d6b27e9d9f8a89d4d819c605a512f44141c
foroozanDANESH <86897389+foroozanDANESH@users.noreply.github.com>
2024-03-06 09:12:23 -0800

    Update index on handledrop for updatewarning.js

--- src/index.js
+++ src/index.js
@@ -1008,7 +1008,7 @@ function handleDrop(event) {
         // Call to update sliders
         updateSliderPosition();
         updateCountabilitySliderPosition();
-
+        updateWarnings(); 
         // Debugging after calling update functions
         console.log("Slider update functions called");
     }
